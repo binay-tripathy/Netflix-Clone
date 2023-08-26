c8d89c97ed1d1665c07037b75588b7c12f4adacf
Binay Tripathy <binaytripathy123@gmail.com>
2023-08-26 22:01:45 +0530

    random

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -13,6 +13,15 @@ export const Header = () => {
           <button>Sign In</button>
         </div>
       </nav>
+      <div className="header-content">
+        <h1>Unlimited movies, TV shows and more.</h1>
+        <h3>Watch anywhere. Cancel anytime.</h3>
+        <p>Ready to watch? Enter your email to create or restart your membership.</p>
+        <form className="email-signup">
+          <input type="email" placeholder='Email address' required />
+          <button type='submit'>Get Started</button>
+        </form>
+      </div>
     </div>
   )
 }
